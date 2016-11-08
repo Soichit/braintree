37cc9c988070d75a51f53fecae3af43d06be0a4b
soichit <soichit@uw.edu>
2016-11-07 19:09:37 -0800

    payment works

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -14,20 +14,4 @@ export class HttpService {
   getData() {
     //return this.http.get('http://localhost:3000/client_token')
   }
-
-  sendData(user: any) {
-    const body = JSON.stringify(user);
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    return this.http.post('https://http://localhost:3000/checkout', body, {
-      headers: headers
-    })
-      .map((data: Response) => data.json())
-      .catch(this.handleError);
-  }
-
-  private handleError (error: any) {
-    console.log(error);
-    return Observable.throw(error.json());
-  }
 }
